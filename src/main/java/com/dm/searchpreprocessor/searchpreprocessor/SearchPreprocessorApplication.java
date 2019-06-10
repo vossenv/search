457ec6911820efd99128331ca73edd7c82794c79
jansseneda-adobe <danimaejanssen@gmail.com>
2019-06-09 19:42:20 -0500

    initializr bare

---
+++ src/main/java/com/dm/searchpreprocessor/searchpreprocessor/SearchPreprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.dm.searchpreprocessor.searchpreprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPreprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPreprocessorApplication.class, args);
+	}
+
+}
